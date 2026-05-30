e4226fee238839d323b8f9a6987d0f77dd1640b0
Anupama <anupamasivasankaran@gmail.com>
2026-05-30 21:03:47 +0200

    frontend api update

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,22 +1,32 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:5000/api/auth";
+const API =
+    "https://smart-resume-system-hf7n.vercel.app/api/auth";
 
 export const loginUser = async (data) => {
     try {
-
         const response = await axios.post(
             `${API}/login`,
             data
         );
 
         return response.data;
-
     } catch (error) {
-
         console.log(error);
-
         throw error;
+    }
+};
 
+export const registerUser = async (data) => {
+    try {
+        const response = await axios.post(
+            `${API}/register`,
+            data
+        );
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 };
